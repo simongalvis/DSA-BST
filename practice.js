f9c4005e22fec74bdf8e7ba2bf87f12fed5f3d91
Simon Galvis <simongalvis9@gmail.com>
2020-12-16 16:46:10 -0700

    Implement 'search' and 'find' functions for BST

--- practice.js
+++ practice.js
@@ -1,1 +1,56 @@
-//Have quiet time.....Seek first the kingdom of God and His righteousness and all these things will be added to you. Matthew 6:33
+//3. Create a BST class
+class BinarySearchTree{
+    constructor(key=null, value=null, parent=null){
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+    insert(key, value){
+        if(this.key == null){
+            this.key = key;
+            this.value = value;
+        }
+        else if(key < this.key){
+            if(this.left == null){
+                this.left = new BinarySearchTree(key, value, this)
+            }
+            else{
+                this.left.insert(key, value);
+            }
+        }
+        else {
+            if (key > this.key){
+            if(this.right == null){
+                this.right = new BinarySearchTree(key, value, this.key, this)
+            }
+            else{
+                this.right.insert(key, value);
+            }
+        }
+      }
+    }
+    find(key){
+        if(this.key === key){
+            return this.value;
+        }
+        else if(key < this.key && this.left){
+            if(key == this.left.key){
+                return this.left.find(key);
+            }
+            
+        }
+        else if(key > this.key && this.right){
+            if(key == this.right.key){
+                return this.right.find(key);
+            }
+            
+        }
+        else{
+            throw new Error('Key not found!')
+        }
+
+    }
+    
+}
